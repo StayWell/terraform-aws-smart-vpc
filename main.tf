f2a6ff22e03793d4fb6a999f867db8298aee0c8b
Jeremiah Norris <jjno91@gmail.com>
2019-11-05 15:29:26 -0600

    DEVOPS-817 adjust cidr calculation for limited azs

--- main.tf
+++ main.tf
@@ -94,7 +94,7 @@ resource "aws_route" "private" {
 resource "aws_subnet" "private" {
   count             = "${var.az_limit ? var.az_count : length(data.aws_availability_zones.this.names)}"
   vpc_id            = "${aws_vpc.this.id}"
-  cidr_block        = "${cidrsubnet("${var.cidr_starting_ip}/16", 4, count.index + var.az_limit ? var.az_count : length(data.aws_availability_zones.this.names))}"
+  cidr_block        = "${cidrsubnet("${var.cidr_starting_ip}/16", 4, count.index + (var.az_limit ? var.az_count : length(data.aws_availability_zones.this.names)))}"
   availability_zone = "${data.aws_availability_zones.this.names[count.index]}"
   tags              = "${merge(map("Name", "${var.env}-private-${count.index}"), map("Type", "Private"), var.tags)}"
 }
